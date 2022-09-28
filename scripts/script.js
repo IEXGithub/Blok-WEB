167c10026000c4f9e67146d2caf0711d55f685ec
IEXGithub <isabelle.eldering@gmail.com>
2022-09-28 13:45:47 +0200

    New Changes 27 sept

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,30 @@
 // JavaScript Document
 console.log("hi");
+var bodyElement = document.querySelector("body");
+
+/******************************/
+/* menu openen de MENU button */
+/******************************/
+
+var openButton = document.querySelector("nav section > button");
+
+openButton.addEventListener("click", openMenu);
+
+function openMenu() {
+    bodyElement.classList.add("toonMenu");
+  }
+
+
+
+/************************************/
+/* menu sluiten met de sluit button */
+/************************************/
+
+var sluitButton = document.querySelector("nav section ul button");
+
+sluitButton.addEventListener("click", sluitMenu);
+
+function sluitMenu() {
+    bodyElement.classList.remove("toonMenu");
+}
+
